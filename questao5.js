8f5cde18774962cd8658bbfbfe3b39c2e785e442
Arlei Nascimento <arleirafael.alvesnascimento@gmail.com>
2024-09-13 14:03:14 -0300

    documentação5

--- questao5.js
+++ questao5.js
@@ -1,9 +1,14 @@
+/*
+1. Recebe uma string e retorna essa string invertida.
+*/
+
 function inverteString(inputString) {
-     let novaString = ''
-     for (let i = inputString.length - 1; i >= 0; i--) {
-          novaString += inputString[i];
-     }
-     return novaString
+  let novaString = "";
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    // Loop do final para o começo da string
+    novaString += inputString[i]; // Adiciona o caractere atual à nova string
+  }
+  return novaString; // Retorna a string invertida
 }
 
-console.log(inverteString("arlei"));
+console.log(inverteString("arlei")); // Exibe a string invertida
